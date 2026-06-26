585f92997a585a7fcc84489936b98feb8bdc624b
nvbayayo <snowykikuki@gmail.com>
2026-06-27 03:25:43 +0530

    Create layout component for RootLayout

---
+++ src/app/layout.jsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "AMANNY STORE",
+  description: "MLBB Diamond Recharge",
+};
+
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
