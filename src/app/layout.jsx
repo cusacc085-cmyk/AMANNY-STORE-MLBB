03ef14ae608c0675c41ad512ecaa3db25a750d9c
nvbayayo <snowykikuki@gmail.com>
2026-06-27 13:16:06 +0530

    Update store description in layout component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,11 @@
-export const metadata = {
-  title: "AMANNY STORE",
-  description: "MLBB Diamond Recharge",
-};
-
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}) {
+export const metadata = {
+  title: "AMANNY STORE",
+  description: "MLBB Recharge Store",
+};
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
