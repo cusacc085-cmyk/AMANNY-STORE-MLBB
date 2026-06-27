c485821a9db486bcbb896c5bf1e8534fc38e053f
nvbayayo <snowykikuki@gmail.com>
2026-06-27 10:42:38 +0530

    Create router.js

---
+++ src/app/api/auth/logout/router.js
@@ -0,0 +1,17 @@
+import {
+  cookies,
+} from "next/headers";
+
+export async function POST() {
+
+  const cookieStore =
+    await cookies();
+
+  cookieStore.delete(
+    "user"
+  );
+
+  return Response.json({
+    success: true,
+  });
+}
